5be2fd836acd2cfcd774cee0323cdf96fadf8637
davi-franca <davifranca629@gmail.com>
2025-09-29 23:51:08 -0300

    'hero-finalizada'

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,24 +1,100 @@
 import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
 
 const Hero = () => {
+
+    useGSAP(() => {
+
+        const heroSplit = new SplitText("#hero-title", {
+            type: "chars, words",
+        });
+
+        const heroSubTitleSplit = new SplitText("#subtitle-hero", {
+            type: "chars, words",
+        });
+
+        gsap.fromTo(
+            "#video-drone",
+            { scale: 1.3 },
+            {
+                scale: 1,
+                duration: 2.0,
+                ease: "power3.out",
+                delay: 1.5,
+                height: "85vh",
+            }
+        );
+
+        // heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+        gsap.from(heroSplit.chars, {
+            scale: 1.3,
+            opacity: 0,
+            delay: 3.0,
+            duration: 2.8,
+            filter: "blur(4px)",
+            ease: "expo.out",
+            stagger: 0.06,
+        });
+
+        gsap.from(heroSubTitleSplit.chars, {
+            scale: 0.8,
+            yPercent: 70,
+            opacity: 0,
+            delay: 1.5,
+            duration: 2.5,
+            ease: "expo.out",
+            stagger: 0.1,
+        });
+
+        gsap.from("#sub2-hero", {
+
+            yPercent: 60,
+            opacity: 0,
+            delay: 3.5,
+            duration: 2.0,
+            ease: "expo.out",
+        });
+
+        gsap.to("#down-button", {
+            yPercent: -10,
+            scale: 1.1,
+            duration: 2.5,
+            opacity: 1,
+            ease: "power1.inOut"
+
+        });
+
+    }, []);
+
     return (
-        <div id='home' className="relative w-screen h-[85vh] overflow-hidden brightness-0.9">
-            <video autoPlay loop muted className="object-cover w-[100%] h-full">
+        //h-[85vh] estava antes
+        <div id='home' className="relative w-screen h-screen overflow-hidden brightness-0.9">
+            <video id='video-drone' autoPlay loop muted className="object-cover w-full h-full">
                 <source src="videos/drone-desktop.mp4" media="(min-width: 768px)" type="video/mp4" />
                 <source src="videos/drone-mobile.mp4" media="(max-width: 767px)" type="video/mp4" />
                 Seu navegador não suporta a tag de vídeo.
             </video>
 
             <div className="font-gothic absolute inset-0 flex items-center justify-center -mt-100 z-20">
-                <h1 className="absolute text-[10rem] text-white font-bold [letter-spacing:0.1em]"> <span className='text-[#e1aa26]'>TB</span>MOTORS</h1>
-                <p className='font-poppins absolute mt-45 text-[20px] [letter-spacing:0.8em]'><span className='text-[#e1aa26]'>EXCLUSIVIDADE</span> EM MOVIMENTO</p>
-                <p className='font-poppins absolute text-[20px] bottom-10 [letter-spacing:0.5em] '>
-                    NÃO PERCA ESSA <span className='text-[#e1aa26]'>OPORTUNIDADE!</span></p>
+                <h1 id='hero-title' className="absolute text-[10rem] text-white font-bold [letter-spacing:0.1em]">
+                    <span className='text-[#e1aa26]'>TB</span>MOTORS
+                </h1>
+                <p id='subtitle-hero' className='font-poppins absolute mt-45 text-[20px] [letter-spacing:0.8em]'>
+                    <span className='text-[#e1aa26]'>EXCLUSIVIDADE</span> EM MOVIMENTO
+                </p>
+                <p id='sub2-hero' className='font-poppins absolute text-[20px] bottom-45 [letter-spacing:0.5em]'>
+                    NÃO PERCA ESSA <span className='text-[#e1aa26]'>OPORTUNIDADE!</span>
+                </p>
             </div>
 
             <button
+                id='down-button'
                 onClick={() => document.getElementById('container1-apresentation')?.scrollIntoView({ behavior: 'smooth' })}
-                className="circle absolute bottom-12 right-1/20 -translate-x-1/2 z-50"
+                className="circle absolute bottom-40 right-1/20 -translate-x-1/2 z-50 opacity-0"
             >
                 <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { navLinks } from '../../constants/index.js';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 const Nav = () => {
+
+    useGSAP(() => {
+
+        gsap.from("nav", {
+            y: -100,           
+            opacity: 0,        
+            duration: 1.0,
+            ease: "power3.out",
+            delay: 3.0         
+        });
+    }, []);
+
     return (
-        <nav className='fixed top-0 left-0 w-full bg-black/70 backdrop-blur-md z-50 h-18'>
+
+        <nav className='fixed top-0 left-0 w-full bg-black/70 backdrop-blur-md z-50 h-18 '>
             <div className=" ml-12 -mt-14">
                 <a href="#home">
                     <img src="/images/logotb.png" alt="logotb" className="w-28 h-38" />
                 </a>
+
+
+
+
 
                 <ul className='flex gap-30 -mr-40 -mt-2 font-gilda'>
                     {navLinks.map((link) => (
